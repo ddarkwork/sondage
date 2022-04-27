c718c41c9d92154c7496069fdf44b6bd343aa683
DDARKWORK <kiforylaminekoneprofessional@gmail.com>
2022-04-27 18:07:22 +0000

    factory & seeders (V1)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Survey::factory(10)->create();
+        \App\Models\Survey::factory(20)->create();
     }
 }

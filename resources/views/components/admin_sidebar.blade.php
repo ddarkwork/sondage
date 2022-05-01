c381ecbf28a847ee054727fa349eaaa85e0b2367
DDARKWORK <kiforylaminekoneprofessional@gmail.com>
2022-05-01 12:49:59 +0000

    Sidebar V2

--- resources/views/components/admin_sidebar.blade.php
+++ resources/views/components/admin_sidebar.blade.php
@@ -1,20 +1,20 @@
 <div class="container-fluid">
     <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark" id="sticky-sidebar">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 color_col" id="sticky-sidebar">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 sticky-top">
                 <img src="{{ asset("images/bigscreen.svg") }}" alt="logo bigscreen" class="logo_position">
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
-                        <a href="{{ route("administration") }}" class="nav-link align-middle px-0">
+                        <a href="{{ route("administration") }}" class="color_link align-middle px-0">
                             <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Accueil</span>
                         </a>
                     </li>
                     <li>
-                        <a href="{{ route("questionnaire") }}" class="nav-link px-0 align-middle">
+                        <a href="{{ route("questionnaire") }}" class="color_link px-0 align-middle">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Questionnaire</span></a>
                     </li>
                     <li>
-                        <a href="{{ route("reponses") }}" class="nav-link px-0 align-middle">
+                        <a href="{{ route("reponses") }}" class="color_link px-0 align-middle">
                             <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Réponses</span> </a>
                     </li>
                 </ul>
